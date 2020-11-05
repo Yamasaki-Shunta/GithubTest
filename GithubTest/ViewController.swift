9e865bbce6674d01baeabea262a67185c9cd834d
Ymasaki-Sunta <syunta@gmail.com>
2020-11-05 22:59:31 +0900

    first commit

---
+++ GithubTest/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GithubTest
+//
+//  Created by 山﨑隼汰 on 2020/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var Animal = "dog"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
